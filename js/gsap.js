908e38873b38de7d185bfb1df4ed517759854b62
Patricia Atanasoff <system.informatica365@gmail.com>
2023-06-26 22:40:16 -0300

    gallery GSAP

---
+++ js/gsap.js
@@ -0,0 +1,30 @@
+const cursor = document.querySelector(".cursor");
+document.body.addEventListener("mousemove", onMouseMove);
+
+function onMouseMove(e) {
+    gsap.to(cursor, 0.0125, {
+        x: e.pageX - 5,
+        y: e.pageY - 5,
+    });
+}
+
+let blocks = document.querySelectorAll(".block");
+let duration = 1;
+let stagger = duration;
+let repeatDelay = 0.075 * (blocks.length - 1);
+
+gsap.from(".block", 5, {
+    scale: 1,
+    top: "50%",
+    left: "50%",
+    transform: "translateZ(-200px)",
+    stagger: {
+        each: duration,
+        repeat: -1,
+        repeatDelay: repeatDelay,
+    },
+});
+
+
+
+        
